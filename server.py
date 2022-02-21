70533b1914083c68673d3f07a77560abd716b8b1
ievaciukore <100118859+ievaciukore@users.noreply.github.com>
2022-02-21 10:35:59 +0200

    Add files via upload

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+@app.route('/',methods = ['POST', 'GET'])
+def root():
+    return render_template("index.html")
+
+@app.route('/health')
+def health():
+    return "OK"
+
+if __name__ == '___main___':
+    app.run(debug="true")
